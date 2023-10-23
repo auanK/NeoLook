265f3dbd3196a02ebb9f3b3f3a2a02e0b3db98e3
Kauan Pablo <kauanpablo089@gmail.com>
2023-10-23 18:58:38 -0300

    Criação da main para a min_heap

--- src/heap.cpp
+++ src/heap.cpp
@@ -1,29 +1,25 @@
+#include <iostream>
+
 #include "../include/min_heap.h"
 
-int main (void) {
+using namespace std;
 
+int main() {
     int vec[] = {4, 2, 8, 7, 1, 5, 3, 6};
     int size = 8;
 
-    min_heap heap;
-    for(int i = 0; i < size; i++){
+    min_heap<int> heap;
+
+    for (int i = 0; i < size; i++) {
         heap.push(vec[i]);
+        cout << endl;
+        heap.printTree(0);
+        cout << endl;
+        cout << "------------------" << endl;
     }
+
     heap.print();
 
-    cout << endl;
-
-    for(int i = 0; i < size; i++){
-        vec[i] = heap.pop();
-    }
-
-    cout << endl;
-
-    for(int i = 0; i < size; i++){
-        cout << vec[i] << " ";
-    }
-
-    cout << endl;
-
-    heap.print();
+    cout << heap.value(7) << endl;
+    return 0;
 }
