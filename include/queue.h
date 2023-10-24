559d0aedf026d2efbf393bbd7a63e7d32e541f98
Kauan Pablo <kauanpablo089@gmail.com>
2023-10-24 08:41:01 -0300

    Add de análise de complexidade da queue

--- include/queue.h
+++ include/queue.h
@@ -2,28 +2,32 @@
 #define QUEUE_H
 #include <stdexcept>
 
-template <typename type>
-
 // Classe que representa um nó da fila.
+template <typename type>
 class node {
    private:
     type value;  // Valor armazenado no nó.
     node *next;  // Ponteiro para o próximo nó.
 
    public:
-    // Construtor que recebe o valor do nó e o ponteiro para o próximo nó.
+    // Construtor que recebe o valor do nó e o ponteiro para o próximo nó. O(1)
     node(const type &value, node *next_ptr) {
         this->value = value;
         next = next_ptr;
     }
 
-    // Retorna o valor do nó.
-    type &get_value() { return this->value; }
+    // Retorna o valor do nó. O(1)
+    type &get_value() { 
+        return this->value; 
+    }
 
-    // Retorna o ponteiro para o próximo nó.
-    node *&get_next() { return this->next; }
+    // Retorna o ponteiro para o próximo nó. O(1)
+    node *&get_next() { 
+        return this->next; 
+    }
 };
 
+// Classe que representa uma fila.
 template <typename type>
 class queue {
    private:
@@ -33,14 +37,14 @@ class queue {
 
    public:
     // Construtor padrão, inicializa o ponteiro para o primeiro e último nó com
-    // nullptr e o tamanho com 0.
+    // nullptr e o tamanho com 0. O(1)
     queue() {
         m_first = nullptr;
         m_last = nullptr;
         m_size = 0;
     }
 
-    // Destrutor da classe, desaloca a memória de todos os nós da fila.
+    // Destrutor da classe, desaloca a memória de todos os nós da fila. O(n)
     ~queue() {
         // Percorre a fila chamando o destrutor de cada nó.
         while (m_first != nullptr) {
@@ -50,13 +54,17 @@ class queue {
         }
     }
 
-    // Retorna o tamanho da fila.
-    unsigned size() const { return m_size; }
+    // Retorna o tamanho da fila. O(1)
+    unsigned size() const { 
+        return m_size; 
+    }
 
-    // Retorna true se a fila estiver vazia, false caso contrário.
-    bool empty() const { return m_size == 0; }
+    // Retorna true se a fila estiver vazia, false caso contrário. O(1)
+    bool empty() const { 
+        return m_size == 0; 
+    }
 
-    // Insere um elemento no final da fila.
+    // Insere um elemento no final da fila. O(1)
     void push(const type &elem) {
         if (m_first == nullptr) {
             m_last = m_first = new node<type>(elem, nullptr);
@@ -67,7 +75,7 @@ class queue {
         m_size++;
     }
 
-    // Remove o elemento do início da fila.
+    // Remove o elemento do início da fila. O(1)
     void pop() {
         if (m_size == 0) {
             return;
@@ -84,15 +92,14 @@ class queue {
         }
     }
 
-    // Retorna o elemento do início da fila.
+    // Retorna o elemento do início da fila. O(1)
     type &front() {
         if (m_size == 0) {
             throw std::runtime_error("fila vazia");
         }
         return m_first->get_value();
     }
-
-    // Retorna o elemento do início da fila(versão const).
+    // Versão constante da função front. O(1)
     const type &front() const {
         if (m_size == 0) {
             throw std::runtime_error("fila vazia");
@@ -100,14 +107,14 @@ class queue {
         return m_first->get_value();
     }
 
+    // Retorna o elemento do final da fila. O(1)
     type &back() {
         if (m_size == 0) {
             throw std::runtime_error("fila vazia");
         }
         return m_last->get_value();
     }
-
-    // Retorna o elemento do final da fila.
+    // Versão constante da função back. O(1)
     const type &back() const {
         // Lança uma exceção se a fila estiver vazia.
         if (m_size == 0) {
