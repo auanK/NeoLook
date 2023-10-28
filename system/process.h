5d45005103af2623e6ae1ad66a4440e54e3e2a6e
Ery Castro <ery.cxtr@gmail.com>
2023-10-28 17:11:09 -0300

    Ajuste na classe "process"

--- system/process.h
+++ system/process.h
@@ -4,11 +4,13 @@
 // classe contendo as demandas dos processsos para cada recurso.
 class process{
     public:
+    int instant;
     int demand_cpu;
     int demand_disk;
     int demand_network;
 
-    process(int demand_cpu, int demand_disk, int demand_network){
+    process(int demand_cpu, int demand_disk, int demand_network, int instant){
+        this->instant = instant;
         this->demand_cpu = demand_cpu;
         this->demand_disk = demand_disk;
         this->demand_network = demand_network;
