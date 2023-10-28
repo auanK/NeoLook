7de5c643e23c7f7aadb6d9a0e42653b7bbfa0b70
Ery Castro <ery.cxtr@gmail.com>
2023-10-28 16:24:56 -0300

    Implementação da classe "process"

---
+++ system/process.h
@@ -0,0 +1,18 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+// classe contendo as demandas dos processsos para cada recurso.
+class process{
+    public:
+    int demand_cpu;
+    int demand_disk;
+    int demand_network;
+
+    process(int demand_cpu, int demand_disk, int demand_network){
+        this->demand_cpu = demand_cpu;
+        this->demand_disk = demand_disk;
+        this->demand_network = demand_network;
+    }
+};
+
+#endif
